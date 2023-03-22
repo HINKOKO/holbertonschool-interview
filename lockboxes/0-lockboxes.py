99da0360450b84b029e066fb7cc3a5958e41a4d6
HINKOKO <piotr62@live.fr>
2023-03-22 15:37:20 +0100

    interview problem yay

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+'''
+Problem ==> given `n` number of boxes, numbered
+from 0 to n - 1, with each box containing hypothetically
+keys to other boxes, ``canUnlockAll`` purpose is to determine
+if all the boxes can be opened
+'''
+
+def canUnlockAll(boxes):
+	'''
+	function to check if all boxes can be opened
+	'''
+	opened = [0]
+
+	for key in opened:
+		for k in boxes[key]:
+			if k < len(boxes) and k not in opened:
+				opened.append(k)
+
+	return True if len(opened) == len(boxes) else False
+
---
+++ lockboxes/README.md
@@ -0,0 +1,9 @@
+# **LockBoxes Problem**
+
+Given 'n' number of boxes locked in front of you, each box is numbered from 0 to 'n - 1' and each box may contain <br>
+a key to the other boxes
+
+**Challenge ==>** write a Python method to check if all boxes can be opened
+- First box is unlocked
+- 'boxes' is a list of lists
+- A key with the same number as a box open that box
