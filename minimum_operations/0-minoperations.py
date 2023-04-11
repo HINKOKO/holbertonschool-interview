898035fbf6f8a4f0701a2d1514695d4e86690741
HINKOKO <piotr62@live.fr>
2023-04-11 23:12:58 +0200

    Using prime factorization

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -2,20 +2,33 @@
 
 """
 Minimum operations module
+Using 'prime factorization' approach
+Number of prime factors --> numbers of operators
+== operations to get n's 'H'
 """
 
 
 def minOperations(n):
-	'''
-	Method to compute the fewest number of operations
-	to reach exactly 'n' times 'H' characters in the file
-	Text editor can only do 'Paste' and 'Copy all' operations
-	'''
-	dp = [float('inf')] * (n + 1)
-	dp[1] = 0
-	for i in range (2, n + 1):
-		for j in range (1, i):
-			if i % j == 0:
-				dp[i] = min(dp[i], dp[j] + i//j)
-	return dp[n]
+    '''
+    Method to compute the fewest number of operations
+    to reach exactly 'n' times 'H' characters in the file
+    Text editor can only do 'Paste' and 'Copy all' operations
+    '''
+    if not isinstance(n, int):
+        return 0
+    if n == 1:
+        return 0
+    ops = 0
+    # 2 smallest prime number
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            # bam => find a prime factor
+            ops += i
+            n /= i
+            # n can't still be divisible by 'i', don't miss that
+            # e.g. 24 /2 = 12 still divisible by 2 so i-=1 i+=1 we check again
+            i -= 1
+        i += 1
+    return int(ops)
 
