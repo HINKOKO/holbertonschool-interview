b0749f013437f4a442b5d0e889aa97e6e134e7e4
HINKOKO <piotr62@live.fr>
2023-04-17 11:49:43 +0200

    modify logic

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -1,65 +1,78 @@
 #include "sandpiles.h"
 
 /**
- * sandpiles_sum - computes the sum of two sandpiles
- * @grid1: sandpile grid 1 (assumed to be individually stable)
- * @grid2: sandpile grid 2 (same base property)
-*/
-
+ * sandpiles_sum - Computes the sum of two sandpiles
+ * @grid1: 3x3 grid to computes
+ * @grid2: 3x3 grid to sum
+ *
+ * Return: Nothing
+ */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int i, j;
-	int computed[3][3];
+	int i = 0, j = 0, toppling = 0;
 
-	for (i = 0; i < GRID_SIZE; i++)
-		for (j = 0; j < GRID_SIZE; j++)
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+		{
 			grid1[i][j] += grid2[i][j];
+			if (grid1[i][j] > 3)
+				toppling = 1;
+		}
 
-	while (!is_stable(grid1))
+	while (toppling == 1)
 	{
-		temp_grid(grid1, computed);
-		print_grid(grid1);
-		topple(grid1);
+		if (toppling == 1)
+			print_grid1(grid1);
+		toppling = topple_check(grid1);
 	}
 }
 
 /**
- * topple - topples plays with sandgrains
- * redistribute them to 4 neighbors, manage 'out
- * of the grid neighbors' and inside neighbors
- * @grid: the grid to topple-lify
- * Return: 1 for success/continue
-*/
-
-int topple(int grid[3][3])
+ * topple_check - check if a "toppling" is needed
+ * and manage neighbords accordingly
+ * @grid1: grid to check the "stability"
+ * Return: 1 for keep toopling, stability not reached yet , 0 otherwise
+ */
+int topple_check(int grid1[3][3])
 {
-	int i, j;
+	int i = 0, j = 0, status = 0;
+	int tmp[3][3] = {0};
 
-	for (i = 0; i < GRID_SIZE; i++)
-		for (j = 0; j < GRID_SIZE; j++)
-			if (grid[i][j] > 3)
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid1[i][j] > 3)
 			{
-				grid[i][j] -= 4;
-				/* manage neighbors exists or not */
+				grid1[i][j] -= 4;
 				if (i > 0)
-					grid[i - 1][j]++;
+					tmp[i - 1][j]++;
 				if (i < 2)
-					grid[i + 1][j]++;
+					tmp[i + 1][j]++;
 				if (j > 0)
-					grid[i][j - 1]++;
+					tmp[i][j - 1]++;
 				if (j < 2)
-					grid[i][j + 1]++;
+					tmp[i][j + 1]++;
 			}
-	return (1);
+		}
+	}
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += tmp[i][j];
+			if (grid1[i][j] > 3)
+				status = 1;
+		}
+	return (status);
 }
 
 /**
- * print_grid - Print 3x3 grid
- * @grid: 3x3 grid
- *
+ * print_grid1 - Print the grid
+ * @grid: the grid to print
  */
 
-static void print_grid(int grid[3][3])
+void print_grid1(int grid[3][3])
 {
 	int i, j;
 
@@ -74,36 +87,4 @@ static void print_grid(int grid[3][3])
 		}
 		printf("\n");
 	}
-}
-
-/**
- * is_stable - check if a grid sandpile is stable
- * @grid: the grid to check on
- * Return: 1 for stable, 0 otherwise
-*/
-int is_stable(int grid[3][3])
-{
-	int i = 0, j = 0;
-
-	for (i = 0; i < GRID_SIZE; i++)
-		for (j = 0; j < GRID_SIZE; j++)
-			if (grid[i][j] > 3)
-				return (FALSE);
-	return (TRUE);
-}
-
-/**
- * temp_grid - holds a copy of the grid at each steps
- * displays it if not stable, to see and understand the calculus
- * @computed: temp grid computed
- * @grid1: the grid we copy
-*/
-
-void temp_grid(int grid1[3][3], int computed[3][3])
-{
-	int i, j;
-
-	for (i = 0; i < GRID_SIZE; i++)
-		for (j = 0; j < GRID_SIZE; j++)
-			computed[i][j] = grid1[i][j];
 }
--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -1,20 +1,12 @@
 #ifndef __SANDPILES__
 #define __SANDPILES__
 
-#define GRID_SIZE 3
-#define FALSE 0
-#define TRUE 1
-/* Switching True to 1 for continuing execution */
-
 #include <stdio.h>
 #include <stdlib.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-static void print_grid(int grid[3][3]);
-int is_stable(int grid[3][3]);
-void temp_grid(int grid1[3][3], int computed[3][3]);
-int topple(int grid[3][3]);
-
+void print_grid1(int grid[3][3]);
+int topple_check(int grid1[3][3]);
 
 
 #endif /* __SANDPILES__ */
