3bde4486793a2d344e5589414bfcdfd08caa29d0
HINKOKO <piotr62@live.fr>
2023-05-04 12:14:11 +0200

    removing pattern, modifying len(line) check

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -9,8 +9,6 @@ import sys
 import re
 
 
-pattern = r'^([\d\.]+) - \[(.*?)\] "GET \/projects\/260 HTTP\/1\.1" ' \
-          r'(\d+) (\d+|-)$'
 total_size = 0
 counter = 0
 codes = ['200', '301', '400', '401', '403', '404', '405', '500']
@@ -21,28 +19,24 @@ count_status = {'200': 0, '301': 0,
 
 try:
     for lines in sys.stdin:
-        match = re.match(pattern, lines.strip())
-        if match:
-            line = lines.split(" ")
-            # print("{}".format(line))
-            # print("###########")
-            # print("{}".format(len(line)))
-            if len(line) == 9:
-                size = line[-1]
-                code = line[-2]
-                if code in count_status:
-                    count_status[code] += 1
-                total_size += int(size)
-                counter += 1
+        line = lines.split(" ")
+        # print("{}".format(line))
+        # print("###########")
+        # print("{}".format(len(line)))
+        if len(line) > 2:
+            size = line[-1]
+            code = line[-2]
+            if code in count_status:
+                count_status[code] += 1
+            total_size += int(size)
+            counter += 1
 
-                if counter == 10:
-                    print("File size: {:d}".format(total_size))
-                    for k, v in sorted(count_status.items()):
-                        if v != 0:
-                            print("{:s}: {:d}".format(k, v))
-                    counter -= 10
-        else:
-            continue
+            if counter == 10:
+                print("File size: {:d}".format(total_size))
+                for k, v in sorted(count_status.items()):
+                    if v != 0:
+                        print("{:s}: {:d}".format(k, v))
+                counter -= 10
 
 except Exception:
     pass
