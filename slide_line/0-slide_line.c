f0209a89776c8378e9f3402dddd8c55243521361
HINKOKO <piotr62@live.fr>
2023-05-31 16:37:15 +0200

    bettylizing and adding docs

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -1,5 +1,13 @@
 #include "slide_line.h"
 
+/**
+ * slide_line - slide an int array in the 2048 game style but on single line
+ * @line: pointer to int array
+ * @size: number of elements in the array
+ * @direction: direction to slide to => Left (-1) Right (1)
+ * Return: 1 success, 0 otherwise
+ */
+
 int slide_line(int *line, size_t size, int direction)
 {
 	int i = 0, j = 0;
@@ -11,7 +19,9 @@ int slide_line(int *line, size_t size, int direction)
 	for (i = (direction == SLIDE_LEFT) ? size - 1 : 0;
 		 (direction == SLIDE_LEFT) ? i >= 0 : (i < (int)size - 1); i += direction)
 	{
-		for (j = i - direction; (direction == SLIDE_LEFT) ? j >= 0 : (j < (int)size - 1); j += direction)
+		for (j = i - direction; (direction == SLIDE_LEFT) ? j >= 0
+														  : (j < (int)size - 1);
+			 j += direction)
 		{
 			if (line[j] == line[i] && line[j] != 0)
 			{
