39a635113efae8fde356724dc1fe77b366d549aa
HINKOKO <piotr62@live.fr>
2023-07-11 15:58:28 +0200

    fixin semistandard spaces

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -4,18 +4,17 @@ const request = require('request');
 const filmNum = process.argv[2];
 
 function printChars (chars, i) {
-  if (i == chars.length) return;
+  if (i === chars.length) return;
   else {
       request(chars[i], function (err, _res, body) {
-          if (err) console.log(err);
-          else {
-              console.log(JSON.parse(body).name);
-              printChars(chars, ++i);
-            }
-        });
-    }
-
-}
+        if (err) console.log(err);
+        else {
+            console.log(JSON.parse(body).name);
+            printChars(chars, ++i);
+        }
+    });
+  }
+};
 
 request.get(`https://swapi-api.hbtn.io/api/films/${filmNum}`, function (err, _res, body) {
   if (err) console.log(err);
