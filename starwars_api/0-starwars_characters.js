f4b255b14c3ffebcb002cc3ed6dee579a4301359
HINKOKO <piotr62@live.fr>
2023-07-11 15:50:27 +0200

    lauching star wars

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+const request = require('request');
+const filmNum = process;argv[2];
+
+function printChars (chars, i) {
+    if (i == chars.length) return;
+    else {
+        request(chars[i], function (err, _res, body) {
+            if (err) console.log(err);
+            else {
+                console.log(JSON.parse(body).name);
+                printChars(chars, ++i);
+            }
+        });
+    }
+
+}
+
+request.get(`https://swapi-api.hbtn.io/api/films/${filmNum}`, function (err, _res, body) {
+    if (err) console.log(err);
+    else {
+        const chars = JSON.parse(body).characters;
+        printChars(chars, 0);
+    }
+});
---
+++ starwars_api/README.md
@@ -0,0 +1,4 @@
+# **Star Wars API**
+
+This project is about to fetch some data from the "SWAPI" Star Wars API. <br>
+We have to use `request` and the code has to be `semistandard` compliant !
