15f9126167575616deff463ca1124600fd2cd20f
HINKOKO <piotr62@live.fr>
2023-10-11 14:16:55 +0200

    men I'm upset now

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -3,6 +3,37 @@
 #include "sort.h"
 
 /**
+ * count_sort - good old count sort, sort
+ * by current digit
+ * @array: array to sort
+ * @size: size of the array
+ * @semi: temp array used to sort working
+ * @pos: current digit position
+ */
+
+void count_sort(int *array, size_t size, int *semi, long pos)
+{
+	size_t i;
+	int bucket[RADIX] = {0};
+
+	/* count the number of 'keys' digits that'll go in each bucket */
+	for (i = 0; i < size; i++)
+	{
+		bucket[(array[i] / pos) % 10]++;
+		semi[i] = 0;
+	}
+
+	for (i = 1; i < RADIX; i++)
+		bucket[i] += bucket[i - 1];
+	/* Use bucket to fill the semi-sorted array */
+	for (i = size - 1; (int)i >= 0; i--)
+		semi[--bucket[(array[i] / pos) % RADIX]] = array[i];
+
+	for (i = 0; i < size; i++)
+		array[i] = semi[i];
+}
+
+/**
  * radix_sort - sorts an array of int ascending order
  * using the radix sort LSD tech
  * @array: the array to sort
@@ -13,8 +44,8 @@ void radix_sort(int *array, size_t size)
 {
 	size_t i = 0;
 	int max = INT_MIN;
-	int bucket[RADIX] = {0}, *semi;
-	int SD = 1;
+	int *semi;
+	long pos;
 
 	if (!array || size < 2)
 		return;
@@ -26,28 +57,10 @@ void radix_sort(int *array, size_t size)
 	for (i = 0; i < size; i++)
 		max = array[i] > max ? array[i] : max;
 
-	while ((max / SD) > 0)
+	for (pos = 1; max / pos > 0; pos *= RADIX)
 	{
+		count_sort(array, size, semi, pos);
 		print_array(array, size);
-
-		/* count the number of 'keys' digits that'll go in each bucket */
-		for (i = 0; i < size; i++)
-			bucket[(array[i] / SD) % 10]++;
-
-		/* add the count of the prevs buckets */
-		/* Acquires the indexes after the end of each bucket location */
-		/* works similar to the count sort algorithm */
-		for (i = 1; i < 10; i++)
-			bucket[i] += bucket[i - 1];
-		/* Use bucket to fill the semi-sorted array */
-		for (i = (int)size - 1; (int)i >= 0; i--)
-			semi[--bucket[(array[i] / SD) % 10]] = array[i];
-
-		for (i = 0; i < size; i++)
-			array[i] = semi[i];
-
-		/* Let's go to next significant digit SD */
-		SD *= 10;
 	}
 	free(semi);
 }
