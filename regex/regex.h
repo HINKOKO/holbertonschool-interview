e416163c75ce185064e8943bbc90ae53ec35a602
HINKOKO <piotr62@live.fr>
2023-11-17 11:02:56 +0100

    Removing the beautiful macro, what a shame

--- regex/regex.h
+++ regex/regex.h
@@ -1,14 +1,6 @@
 #ifndef __REGEX__
 #define __REGEX__
 
-/* Macro directive for lauching the tests */
-#define TEST_MATCH(s, p) do { \
-	{ \
-		int res = regex_match(s, p);\
-		printf("%s -> %s = %d\n", s, p, res);\
-	} \
-} while (0)
-
 #include <stdio.h>
 #include <stdlib.h>
 
